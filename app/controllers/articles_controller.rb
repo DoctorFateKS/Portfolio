072412b6a0f3acebf4bc3645c5958a2f8b1c514f
Kevin Saison <kevin.saison91@gmail.com>
2025-04-17 11:33:00 +0200

    Implementing the show & new action for Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     @articles = Article.order(:title).limit(5)
   end
+
+  def show
+    @article = Article.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "Article not found."
+    redirect_to articles_path
+  end
+
+  def new
+    @article = Article.new
+  end
 end
