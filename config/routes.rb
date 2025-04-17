e5dbdd5077d8776d97f72e5183f55d2f51eebe70
Kevin Saison <kevin.saison91@gmail.com>
2025-04-17 19:37:11 +0200

    Configuring routes & creating basic view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'contact/new'
-  get 'contact/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,5 +11,6 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :contacts, only: [:new, :create]
 
 end
