3a4e139fd7e19f2cc0ce077ba732579f89376e0d
Kevin Saison <kevin.saison91@gmail.com>
2025-04-18 14:15:12 +0200

    Implementing basic form validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :description, presence: true
+  validates :live_link, format: { with: URI::regexp(%w[http https]), allow_blank: true }
+  validates :source_code_link, format: { with: URI::regexp(%w[http https]), allow_blank: true }
 end
