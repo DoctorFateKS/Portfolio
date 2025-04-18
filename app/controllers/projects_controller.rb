4af9bf94484e7fe9b918c6423120e1b7af2828b7
Kevin Saison <kevin.saison91@gmail.com>
2025-04-18 14:12:46 +0200

    Buiding the new project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,23 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :image_url, :live_link, :source_code_link)
+  end
 end
