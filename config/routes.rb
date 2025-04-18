5259a89e2e9310e4919d436e48e7af7af09b8d4a
Kevin Saison <kevin.saison91@gmail.com>
2025-04-18 13:30:45 +0200

    Implementing basic contact page

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,4 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :contact, only: [:new, :create]
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_17_113237) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_17_191751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_17_113237) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
